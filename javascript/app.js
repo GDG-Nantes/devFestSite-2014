30cad384f68a37b8479f265f1eb4c7b4d96b5ef4
jefBinomed <jean.francois.garreau@gmail.com>
2014-04-11 23:37:53 +0200

    mise a jour du site pour mettre de cote l'aspect codelab et mettre en place une image de chargement ! Attention un npm install sera a nouveau necessaire car j'ai update les versions de compass

--- javascript/app.js
+++ javascript/app.js
@@ -9,33 +9,48 @@ var DevFest = DevFest || function(){
       document.getElementById('unsupport-div').style['display'] = 'block';
       document.getElementById('content').style['display'] = 'none';
     }else{
-      jQuery(document).ready(function() {
-        setTimeout(function() {
-          jQuery.fn.fullpage({
-            verticalCentered: true,
-                resize : false,
-                slidesColor: ['#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2'],
-            anchors: ['slide01', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
-                scrollingSpeed: 700,
-                easing: 'easeInQuart',
-                menu: true,
-                navigation: true,
-                navigationPosition: 'top',
-                slidesNavigation: false,
-                slidesNavPosition: 'bottom',
-                loopBottom: false,
-                loopTop: false,
-                loopHorizontal: true,
-                autoScrolling: true,
-                scrollOverflow: false,
-                css3: false,
-                keyboardScrolling: true,
-                touchSensitivity: 15,
-                animateAnchor: true
-          });
-        }, 1000);
+
+      // Chargement asynchrone des composans histoire d'afficher l'animation de chargement
+      loadComponent("partials/contacts.html");
+      loadComponent("partials/content.html");
+      loadComponent("partials/home.html");
+      loadComponent("partials/sponsoring.html");
+      loadComponent("partials/what_is_devfest.html");
+      loadComponent("partials/when_where_how.html");
+      window.addEventListener('WebComponentsReady', function() {
+        document.getElementById('contain').style['display'] = 'none';
+        jQuery.fn.fullpage({
+          verticalCentered: true,
+          resize : false,
+          slidesColor: ['#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2'],
+          anchors: ['slide01', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
+          scrollingSpeed: 700,
+          easing: 'easeInQuart',
+          menu: true,
+          navigation: true,
+          navigationPosition: 'top',
+          slidesNavigation: false,
+          slidesNavPosition: 'bottom',
+          loopBottom: false,
+          loopTop: false,
+          loopHorizontal: true,
+          autoScrolling: true,
+          scrollOverflow: false,
+          css3: false,
+          keyboardScrolling: true,
+          touchSensitivity: 15,
+          animateAnchor: true
         });
+      });     
     }
+  }
+
+  function loadComponent(filename){
+    var fileref=document.createElement("link");
+    fileref.setAttribute("rel", "import");    
+    fileref.setAttribute("href", filename);   
+    if (typeof fileref!="undefined")
+      document.getElementsByTagName("head")[0].appendChild(fileref)  
   }
 
   init();

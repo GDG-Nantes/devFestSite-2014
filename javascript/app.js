7a8681e579438951a333544114cb55ae645b13e9
Benjamin Petetot <bpetetot@gmail.com>
2014-04-13 18:01:26 +0200

    Nouvelle version avec les sponsors

--- javascript/app.js
+++ javascript/app.js
@@ -6,42 +6,60 @@ var DevFest = DevFest || function(){
   function init(){
 
     if (window['Polymer']  === undefined){
+
       document.getElementById('unsupport-div').style['display'] = 'block';
       document.getElementById('content').style['display'] = 'none';
-    }else{
 
-      // Chargement asynchrone des composans histoire d'afficher l'animation de chargement
+    } else {
+
+      // Chargement asynchrone des composants le temps de l'animation de chargement
       loadComponent("partials/contacts.html");
       loadComponent("partials/content.html");
       loadComponent("partials/home.html");
       loadComponent("partials/sponsoring.html");
+      loadComponent("partials/sponsors.html");
       loadComponent("partials/what_is_devfest.html");
-      loadComponent("partials/when_where_how.html");
+
       window.addEventListener('WebComponentsReady', function() {
+        
+        // Load FullPage
         document.getElementById('contain').style['display'] = 'none';
-        jQuery.fn.fullpage({
-          verticalCentered: true,
-          resize : false,
-          slidesColor: ['#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2'],
-          anchors: ['slide01', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
-          scrollingSpeed: 700,
-          easing: 'easeInQuart',
-          menu: true,
-          navigation: true,
-          navigationPosition: 'top',
-          slidesNavigation: false,
-          slidesNavPosition: 'bottom',
-          loopBottom: false,
-          loopTop: false,
-          loopHorizontal: true,
-          autoScrolling: true,
-          scrollOverflow: false,
-          css3: false,
-          keyboardScrolling: true,
-          touchSensitivity: 15,
-          animateAnchor: true
+          jQuery.fn.fullpage({
+            verticalCentered: false,
+            resize : true,
+            slidesColor: ['#444', '#DDD', '#f2f2f2', '#f2f2f2', '#444', '#f2f2f2', '#f2f2f2', '#444'],
+            anchors: ['#home', '#what-is-devfest', 't1', '#devfest-content', 't2', '#sponsoring', '#sponsors', 't3', '#contacts'],
+            autoScrolling: false,
+            scrollOverflow: false,
+            css3: true,
+            animateAnchor: true,
+            keyboardScrolling: true,
+            touchSensitivity: 15,
+            animateAnchor: false
         });
-      });     
+
+
+        // Load Maps
+        var map_canvas = document.getElementById('map_canvas');
+        
+        var myOptions = {
+          zoom:15,
+          center:new google.maps.LatLng(47.2139677,-1.5430134000000635),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(map_canvas, myOptions);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: new google.maps.LatLng(47.2139677, -1.5430134000000635)
+        }); 
+
+        var infowindow = new google.maps.InfoWindow({content:"<span style='height:auto !important; display:block; white-space:nowrap; overflow:hidden !important;'><strong style='font-weight:400;'>DevFest Nantes 2014</strong><br>Cit&eacute; de congr&egrave;s,  Quai Ferdinand Favre<br> Nantes</span>" }); 
+        google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});
+        infowindow.open(map,marker);
+
+      });  
+
     }
   }
 
